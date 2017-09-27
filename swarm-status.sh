c44eca4187954c4b21351c13b2992f0f8b85e126
bachner <bachner@users.noreply.github.com>
2017-09-27 11:38:44 +0300

    Update swarm-status.sh

--- swarm-status.sh
+++ swarm-status.sh
@@ -1,3 +1,3 @@
-#!/bin/bash env
+#!/bin/bash
 docker node ls | grep Ready | wc -l | awk '{print "Number of docker nodes: " $1}'
 docker stack ls | awk '{print $2}' | grep [0-9]* | sort | uniq -c | awk '{s+=$1*$2; print "- " $1 " stacks of " $2 " containers"} END {print "Total running containers: " s}'
